1361dec4f724bd30c6f48cd46671a4f64fa9bc3d
lihaijian <lihaijian@sythealth.com>
2018-12-13 17:51:21 +0800

    no message

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,14 @@
 const path = require('path')
-const glob = require('glob')
 const utils = require('./build/utils')
+
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
 const resolve = dir => {
   return path.join(__dirname, dir)
 }
+
+const isPro = process.env.NODE_ENV === 'production'
 
 let baseUrl = '/vue';
 
@@ -18,5 +22,24 @@ module.exports = {
       .set('_com', resolve('src/components'))
       .set('_img', resolve('src/images'))
       .set('_ser', resolve('src/services'))
+  },
+  configureWebpack: config => {
+    if (isPro) {
+      // 生产环境修改配置
+      return {
+        plugins: [
+          new CompressionWebpackPlugin({
+            algorithm: 'gzip',
+            test: new RegExp(
+              '\\.(js|css)$'
+            ),
+            threshold: 10240,
+            minRatio: 0.8
+          }),
+          // 使用包分析工具
+          new BundleAnalyzerPlugin()
+        ]
+      }
+    }
   }
 }
