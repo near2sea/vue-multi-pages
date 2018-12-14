0ccce5bde351897ab678c85e81d263b29229a53f
lihaijian <lihaijian@sythealth.com>
2018-12-14 09:31:50 +0800

    no message

--- src/views/oldinvertnew/components/About.vue
+++ src/views/oldinvertnew/components/About.vue
@@ -1,5 +1,29 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="wrapper">
+    <ul>
+      <li v-for="(item, index) in items"
+          :key="index">{{item}}</li>
+    </ul>
   </div>
 </template>
+
+<script>
+export default {
+  components: {},
+  props: {},
+  data () {
+    return {
+      items: []
+    }
+  },
+  watch: {},
+  computed: {},
+  methods: {},
+  created () { },
+  mounted () { }
+}
+</script>
+<style lang="scss" scoped>
+.wrapper {
+}
+</style>
