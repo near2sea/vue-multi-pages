acbb549cf799ca47236f2424d352c1f0bcca6e82
lihaijian <sanan.li@qq.com>
2018-12-20 23:02:19 +0800

    no message

---
+++ src/views/modelbind/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="wrapper">
+    <child v-model="value"></child>
+  </div>
+</template>
+
+<script>
+import Child from './Child'
+export default {
+  components: {
+    Child
+  },
+  props: {},
+  data () {
+    return {
+      value: 66
+    }
+  },
+  watch: {},
+  computed: {},
+  methods: {},
+  created () { },
+  mounted () { }
+}
+</script>
+<style lang="scss" scoped>
+.wrapper {
+  height: 100%;
+}
+</style>
---
+++ src/views/modelbind/Child.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="wrapper">
+    {{ currentValue }}
+    <button @click="handleClick">Click</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: Number,
+      default: 0
+    }
+  },
+  data () {
+    return {
+      currentValue: this.value
+    }
+  },
+  methods: {
+    handleClick () {
+      this.currentValue += 1;
+      this.$emit('input', this.currentValue);
+    }
+  },
+  watch: {
+    value (val) {
+      this.currentValue = val;
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.wrapper {
+  width: 100%;
+}
+</style>
