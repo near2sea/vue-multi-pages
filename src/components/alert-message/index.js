612fed29ec6bd742662768d329dc02fff9b76067
lihaijian <lihaijian@sythealth.com>
2018-12-25 17:44:13 +0800

    no message

--- src/components/alert-message/index.js
+++ src/components/alert-message/index.js
@@ -1,8 +1,14 @@
 import AlertMessage from './main.js'
 
-AlertMessage.install = function(Vue, opts) {
+AlertMessage.install = function (Vue, opts) {
   console.info(opts)
   Vue.prototype.$alertMessage = AlertMessage.alert
 }
 
 export default AlertMessage
+
+// export default {
+//   install(Vue) {
+//     Vue.prototype.$toast = Toast;
+//   }
+// }
